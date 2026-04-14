4be389298605b7c7f49586139968e1e65534882c
Barkın <114268860+barkinbaydar06@users.noreply.github.com>
2026-04-14 16:48:33 +0300

    prompt polish

--- utils/ai_conversation.py
+++ utils/ai_conversation.py
@@ -21,6 +21,7 @@ Kurallar:
 6. Aday cevap vermezse veya "(Pas geçildi)" derse, yorum yapmadan geç.
 7. Takip sorularında adayın cevabındaki eksik veya belirsiz noktaları sorgula.
 8. Her soruyu kesinlikle oku, diğer soruya geçeceksen geçiş cümlesi kullandıktan sonra diğer soruyu kesinlikle oku.
+9. Her sorudan sonra aday tarafından verilen cevabı özetleme veya tekrar etme.
 
 Örnek geçiş cümleleri (kısa tut bunlarla sınırlı kalmak zorunda değilsin):
 - "Anladım. Peki şunu sormak istiyorum..."
